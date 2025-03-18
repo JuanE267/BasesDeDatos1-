ced2281f57c1d49b27b27a41bd21df695e724645
Juan Espinosa Unlu <juanicsp2003@gmail.com>
2025-03-18 14:05:48 -0300

    Crear metadata, Crear campo, Mostrar campos
    Implementacion parcial de:
    - Crear metadata
    - Alta de campo
    - Mostrar  campos

---
+++ TP1/main.c
@@ -0,0 +1,89 @@
+#include <unistd.h> // For getcwd
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct metadata {
+   int numero;
+   char descripcion[30];
+   char tipo; // int O string (i o s)
+   int longitud;
+} metadata;
+
+void altaDeCampo();
+void crearArchivoMetadata();
+void mostrarArchivoMetadata();
+
+int main(void) {
+
+   int eleccion;
+   printf("1. Crear archivo metadata\n");
+   printf("2. Crear nuevo campo(alta)\n");
+   printf("3. Mostrar archivo metadata\n");
+   printf("Ingrese una operacion:  ");
+   scanf("%d", &eleccion);
+
+   if (eleccion == 1) {
+      crearArchivoMetadata();
+   } else if (eleccion == 2) {
+      altaDeCampo();
+   }else if (eleccion == 3) {
+      mostrarArchivoMetadata();
+   }
+
+   return 0;
+}
+
+void crearArchivoMetadata() {
+   FILE* fptr = fopen("metadata.txt", "w");
+
+   if (fptr != NULL) {
+      printf("Archivo de metadata creado.\n");
+   } else {
+      printf("Error el crear el archivo de metadata\n");
+   }
+   fclose(fptr);
+}
+void altaDeCampo() {
+
+   FILE* fptr = fopen("metadata.txt", "a");
+
+   // en cada linea de la metadata debo definir numero desc tipo y long
+   metadata data;
+
+   //data numero
+   printf("Ingrese el numero: ");
+   scanf("%d", &data.numero);
+   //data descripcion
+   printf("Ingrese la descripcion: ");
+   scanf("%s", data.descripcion);
+   //data tipo
+   printf("Ingrese el tipo: ");
+   scanf(" %c", &data.tipo);
+   //data longitud
+   printf("Ingrese la longitud: ");
+   scanf("%d", &data.longitud);
+
+   fprintf(fptr, "%d,%s,%c,%d \n", data.numero, data.descripcion, data.tipo, data.longitud);
+
+   fclose(fptr);
+
+}
+void mostrarArchivoMetadata() {
+   FILE* fptr = fopen("metadata.txt", "r");
+
+   printf("---------- Campos ----------\n");
+   metadata data;
+   char buffer[100];
+
+   while (fgets(buffer, 100, fptr)) {
+      sscanf(buffer, "%d,%[^,], %c,%d", &data.numero, data.descripcion, &data.tipo, &data.longitud);
+
+      printf("Numero -> %d\n", data.numero);
+      printf("Descripcion -> %s\n", data.descripcion);
+      printf("Tipo -> %c\n", data.tipo);
+      printf("Longitud -> %d\n", data.longitud);
+      printf("----------------------------\n");
+   };
+   fclose(fptr);
+}
